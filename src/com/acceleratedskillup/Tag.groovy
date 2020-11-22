6ab44f6a12c35bd46a8f279b1afc0d2671dd411f
rasberrypie <32066845+rasberrypie@users.noreply.github.com>
2020-11-22 19:19:38 +0530

    Update Tag.groovy

--- src/com/acceleratedskillup/Tag.groovy
+++ src/com/acceleratedskillup/Tag.groovy
@@ -1,9 +1,19 @@
 package com.acceleratedskillup;
 
-def generateImageTag(String env, String version) { 
+def generateImageTag(String env, String version) {
    sh "git rev-parse --short HEAD > .git/commit-id"               
    commit_id = readFile('.git/commit-id').trim()
-   newTag = "${version}-${env}-${commit_id}"
+   
+   if (env.BRANCH_NAME == "master") {
+      newTag = "${version}-master-${commit_id}"
+   }
+   if (env.BRANCH_NAME == "qa") {
+      newTag = "${version}-qa-${commit_id}"
+   }
+   if (env.BRANCH_NAME != "master" && env.BRANCH_NAME != "qa") {
+      newTag = "${version}-dev-${commit_id}"
+   }
    sh "echo ${newTag}"
    return "${newTag}"
+   
 }
