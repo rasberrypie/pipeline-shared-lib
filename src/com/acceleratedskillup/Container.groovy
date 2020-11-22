f50514f6b8a0d96e59fb211da02ce54183ccd248
rasberrypie <32066845+rasberrypie@users.noreply.github.com>
2020-11-22 16:23:49 +0530

    Update Container.groovy

--- src/com/acceleratedskillup/Container.groovy
+++ src/com/acceleratedskillup/Container.groovy
@@ -1,7 +1,7 @@
 package com.acceleratedskillup;
 
-def build(commit_id) {
+def build(String commit_id) {
   docker.withRegistry('https://index.docker.io/v1/', 'dockerhub') {
-  def app = docker.build("wakeupcolumn/jenkinsrepo:${commit_id}", '.').push()
+    def app = docker.build("wakeupcolumn/jenkinsrepo:${commit_id}", '.').push()
   }
 }
