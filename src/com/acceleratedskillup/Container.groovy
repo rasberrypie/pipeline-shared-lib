0298fdf6db97a9b33dd7e1a1700557c92450f4df
rasberrypie <32066845+rasberrypie@users.noreply.github.com>
2020-11-22 17:09:17 +0530

    Update Container.groovy

--- src/com/acceleratedskillup/Container.groovy
+++ src/com/acceleratedskillup/Container.groovy
@@ -1,7 +1,7 @@
 package com.acceleratedskillup;
 
-def buildImage(String commitId, String imageName, String filePath = ".") {
+def buildImage(String imageTag, String imageName, String filePath = ".") {
   docker.withRegistry('https://index.docker.io/v1/', 'dockerhub') {
-    def app = docker.build("wakeupcolumn/${imageName}:${commitId}", "${filePath}").push()
+    def app = docker.build("wakeupcolumn/${imageTag}:${commitId}", "${filePath}").push()
   }
 }
